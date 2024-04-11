87294bf009c60e3c51f6138b1c59f1ccc3a64d7c
Elaine Ferreira <elaineferreira.dev@gmail.com>
2024-04-10 23:13:10 -0300

    Adicionar exercícios da aula 01

---
+++ estrutura-de-dados-pesquisa-ordenacao/exercicios/aula01/var-e-const.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#define PI 3.1415
+#define texto "Isso e um teste..."
+
+int main(){
+  int qtde;
+  float n1, n2;
+  int idade = 18;
+  char nome[10] = "Ferreira";
+
+  printf("PI = %f\n", PI);
+  printf("%s\n", texto);
+  printf("Qtde = %d\n", qtde);
+  printf("N1 e N2 = %d\n", n1, n2);
+  printf("Idade = %d\n", idade);
+  printf("Nome = %s\n", nome);
+}

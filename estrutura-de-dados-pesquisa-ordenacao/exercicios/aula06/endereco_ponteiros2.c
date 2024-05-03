ad1b874baf232402994aab40df261290651e9c8b
Elaine Ferreira <elaineferreira.dev@gmail.com>
2024-05-02 21:30:48 -0300

    Adiciona exercicios sobre ponteiros

--- estrutura-de-dados-pesquisa-ordenacao/exercicios/aula06/endereco_ponteiros2.c
+++ estrutura-de-dados-pesquisa-ordenacao/exercicios/aula06/endereco_ponteiros2.c
@@ -5,11 +5,16 @@ int main(){
   int num;
   int *ptr = NULL;
 
+  //atribui o endereço de num ao ptr
   ptr = &num;
 
   printf("Digite um numero inteiro: ");
-  scanf("%d", ptr);
+  scanf("%d", ptr); //guarda no ptr
+  //prt armazena o endereco de num
+  //logo o valor é guardado em num
 
+  //como ptr referencia o endereço de num 
+  //num guarda o endereço digitado
   printf("O valor de num é: %d\n", num);
 
   return 0;
